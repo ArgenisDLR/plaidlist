457dc4f18e09e57ece0571690058dc3476e2c7ef
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-03-28 15:57:01 -0400

    info.plist access to media player

--- plaidlist/GenreButtonScreenViewController.swift
+++ plaidlist/GenreButtonScreenViewController.swift
@@ -7,24 +7,41 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class GenreButtonScreenViewController: UIViewController {
+
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        playGenre(genre: sender.currentTitle!)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
     }
-    */
-
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String) {
+        musicPlayer.stop()
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+    
 }
